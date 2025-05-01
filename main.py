1beaf87e99625f03922a1f5f499dc359539afe4c
maneeshasahas1234 <116358625+maneeshasahas1234@users.noreply.github.com>
2025-05-01 21:39:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+import streamlit as st
+
+
+st.title('Home Price Prediction')
+
+model = joblib.load('model.pkl')
+
+area = st.number_input("Area (sq ft)", value=1000)
+bedrooms = st.number_input("Number of Bedrooms", value=2, step=1)
+age = st.number_input("Age of House (years)", value=10)
+
+if st.button('Predict'):
+    prediction = model.predict([[area, bedrooms, age]])
+    st.success(f"Estimated House Price: ${prediction[0]:,.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
